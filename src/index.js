00d0e831b2a974284b8076d60821bc58b1552ba8
harryge00 <harryge00@gmail.com>
2018-01-03 23:28:54 +0800

    update code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ class MyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            lowBTCvalue: 0,
+            highBTCvalue: 99999,
             error: null,
             isLoaded: false,
             messages: []
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log("handleChange");
+    }
+
+    handleSubmit(event) {
+        console.log("handleSubmit");
     }
 
     componentDidMount(){
@@ -16,13 +28,16 @@ class MyComponent extends React.Component {
         this.connection = new WebSocket('wss://real.okex.com:10440/websocket/okexapi');
         this.connection.onmessage = evt => {
             // listen to onmessage event
-            console.log(evt.data);
-            if(evt.data[0].data) {
-                alert(evt.data[0].data.last)
+            var obj = JSON.parse(evt.data);
+            if(obj[0]) {
+                console.log(obj[0].data.last);
+                if(obj[0].data.last > 15100) {
+                    notifyMe(JSON.stringify(obj[0].data));
+                }
             }
             // add the new message to state
             this.setState({
-                messages : this.state.messages.concat([ evt.data ])
+                messages : this.state.messages.concat([evt.data])
             });
         };
         // var senddata = {'event':'addChannel','channel':'ok_sub_spot_usd_btc_ticker','binary','1'}
@@ -39,8 +54,47 @@ class MyComponent extends React.Component {
 
     render() {
         // render the messages from state:
-        return <ul>{ this.state.messages.map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul>;
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    lowBTCvalue:
+                    <input type="text" value={this.state.lowBTCvalue} onChange={this.handleChange} />
+                    highBTCvalue:
+                    <input type="text" value={this.state.highBTCvalue} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            <ul>{ this.state.messages.map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul>
+            </div>
+        );
     }
+}
+
+function notifyMe(text) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        new Notification(text);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                new Notification(text);
+            }
+        });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
 }
 
 ReactDOM.render(
